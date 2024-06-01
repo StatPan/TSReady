20a72d00ccd64f9c4a4da776f532d66218497f0a
statpan <statpan@naver.com>
2024-06-01 19:39:04 +0900

    chore: trying to show the top level await, with main.ts

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,3 +1,5 @@
-import { helloWorld } from "./util.ts";
-const res = await helloWorld()
-console.log(res)
+export {};
+
+const res = await fetch("https://www.naver.com");
+const resText = await res.text();
+console.log(resText);
